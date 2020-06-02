5d7e6ed5aa96ab4ac8f1a0ecd84e964bc3079ead
Mantaks <50716750+Mantaks@users.noreply.github.com>
2020-06-02 21:19:30 +0300

    firs commit

---
+++ PostClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+
+namespace PostClient
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            var value = new WeatherForecast { TemperatureC = 333 };
+            string json = JsonConvert.SerializeObject(value);
+            var body = Encoding.UTF8.GetBytes(json);
+            var request = (HttpWebRequest)WebRequest.Create("https://localhost:44360/weatherforecast");
+
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = body.Length;
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                stream.Write(body, 0, body.Length);
+                stream.Close();
+            }
+
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                Console.WriteLine(reader.ReadToEnd());
+            }
+            Console.ReadKey();
+        }
+    }
+}
